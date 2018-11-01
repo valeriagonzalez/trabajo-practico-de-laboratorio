024eb58525bd0479284f753437833563818f6e7e
Santiago Gimenez <42920751+Chiqui1234@users.noreply.github.com>
2018-11-01 16:44:45 -0300

    Create main.cpp
    Archivo principal de nuestro software

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "funcion.h"
+using namespace std;
+int main() {
+	// Inicia el menú principal
+	cout << ":: CONTROLADOR DE GARAGE ::\nDigite la opción a seleccionar:" << endl;
+	cout << "1. Ingresar un automóvil por 24hrs o menos" << endl;
+	cout << "2. Ingresar un automóvil hasta una fecha específica" << endl; // Tomaremos la fecha del sistema, el "hasta" será determinado por el usuario
+	cout << "3. Cambiar automóvil de cochera" << endl;
+	cout << "4. Liberar una cochera fija" << endl;
+	cout << "5. Listar deudores" << endl;
+	cout << "6. Listar las cobranzas mensuales" << endl;
+	// Finaliza el menú principal
+}
