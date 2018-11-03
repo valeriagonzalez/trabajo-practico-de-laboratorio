874ebdfa8572e5345313aa8e0c0176355147e859
Santiago Gimenez <42920751+Chiqui1234@users.noreply.github.com>
2018-11-03 15:06:34 -0300

    funcion.cpp NUEVO

--- funcion.cpp
+++ funcion.cpp
@@ -1,3 +1,9 @@
-// Este archivo está vacío, por ahora :P
 #include <iostream>
 using namespace std;
+#include "funcion.h"
+void lotePreSeteado(){
+  //ST_DUENO.nombre="Santiago";
+  //ST_DUENO.apellido="Agustin";
+  ST_COCHERA Cochera;
+  Cochera.id = 2;
+}
