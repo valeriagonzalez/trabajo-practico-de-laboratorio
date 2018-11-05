d0a7f22a84b4a6347ce49418da67d9bb680ee225
Santiago Gimenez <42920751+Chiqui1234@users.noreply.github.com>
2018-11-05 19:50:24 -0300

    ¡Punto 1 (Ingresar cochera) terminado!
    ¡Punto 1 (Ingresar cochera) terminado! Ingresa un vehículo a la cochera junto a su dueño en la estructura

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 #include "funcion.h"
 using namespace std;
 int main() {
+	int opcion;
 	// Inicia el menú principal
 	cout << ":: CONTROLADOR DE GARAGE ::\nDigite la opción a seleccionar:" << endl;
-	cout << "1. Ingresar un automóvil por 24hrs o menos" << endl;
-	cout << "2. Ingresar un automóvil hasta una fecha específica" << endl; // Tomaremos la fecha del sistema, el "hasta" será determinado por el usuario
-	cout << "3. Cambiar automóvil de cochera" << endl;
-	cout << "4. Liberar una cochera fija" << endl;
-	cout << "5. Listar deudores" << endl;
-	cout << "6. Listar las cobranzas mensuales" << endl;
+	cout << "1. Ingresar un automóvil" << endl;
+	cout << "2. Cambiar automóvil de cochera" << endl;
+	cout << "3. Liberar una cochera fija" << endl;
+	cout << "4. Listar deudores" << endl;
+	cout << "5. Listar las cobranzas mensuales" << endl;
 	// Finaliza el menú principal
+	cout << "\nOpción: ";
+	cin >> opcion;
 	//lotePreSeteado();
-	ingresarUnAutomovil();
+	if(opcion==1){
+		ingresarUnAutomovil();
+	}
 }
